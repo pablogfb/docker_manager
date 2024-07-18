de64e47e74749631c98afae641f465032d2bb5a4
pablo <pablobuntu@gmail.com>
2024-07-18 14:28:31 +0200

    Container show and del

--- app/controllers/v1/containers_controller.rb
+++ app/controllers/v1/containers_controller.rb
@@ -15,17 +15,36 @@ class V1::ContainersController < ApplicationController
       render json: { status: { code: 503 }, message: error.message  }, status: 503
     end
   end
+  
 
   def create
     #TODO: docker container run
   end
 
+
   def show
-    #TODO: docker container inspect / logs
+    begin
+      container = Docker::Container.get(params[:id])
+      render json: { status: { code: 200 }, data: {container: container, logs: container.logs(stdout: true) } }
+    rescue Docker::Error::NotFoundError
+      render json: { status: { code: 404 }, message: 'Docker container not found' }, status: :not_found
+    rescue => error
+      render json: { status: { code: 503 }, message: error.message  }, status: 503
+    end
   end
 
+
   def destroy
-    #TODO: docker container rm
+    begin
+      container = Docker::Container.get(params[:id])
+      container.stop
+      container.remove
+      render json: { status: { code: 200 }, message: 'Container deleted' }
+    rescue Docker::Error::NotFoundError
+      render json: { status: { code: 404 }, message: 'Docker Container not found'  }, status: :not_found
+    rescue => error
+      render json: { status: { code: 503 }, message: error.message  }, status: 503
+    end
   end
 
 
--- spec/requests/v1/containers_spec.rb
+++ spec/requests/v1/containers_spec.rb
@@ -1,18 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe "V1::Containers", type: :request do
-  describe "GET /index" do
-    describe "GET /" do
-      it "returns success" do
-        
-        get "/v1/containers"
-        expect(response.status).to eq(200)
-        
-        # Return Docker images list
-        data = JSON.parse(response.body)["data"]
-        expect(data).to match_json_schema("containers")
-              
-      end
+  describe "GET /" do
+    it "returns success" do
+      
+      get "/v1/containers"
+      expect(response.status).to eq(200)
+      
+      # Return Docker images list
+      data = JSON.parse(response.body)["data"]
+      expect(data).to match_json_schema("containers")
+            
     end
+  end
+
+  describe "GET /:id" do
+
+    it "returns 404 if the container does not exist" do 
+      
+      get "/v1/containers/nonexistingcontainer"
+
+      expect(response.status).to eq(404)
+      expect(response.body).to eq('{"status":{"code":404},"message":"Docker container not found"}')
+    end
+
+    it "returns the container info if the container exists" do 
+      
+      image = Docker::Image.build("FROM alpine\nRUN echo 'Hello, World!' > /test.txt ")
+      container = Docker::Container.create("Image": image.id)
+
+      get "/v1/containers/#{container.id}"
+      expect(response.status).to eq(200)
+      expect(response).to match_json_schema("container")
+      container.remove(:force => true)
+      image.remove(:force => true)
+
+    end
+    
+  end
+
+
+  describe "DELETE /:id" do 
+    it "returns 404 if the container does not exist" do 
+      
+      delete "/v1/containers/nonexistingcontainer"
+
+      expect(response.status).to eq(404)
+      expect(response.body).to eq('{"status":{"code":404},"message":"Docker Container not found"}')
+    end
+
+
+    it "returns success and delete the container if the container exists and is not running" do 
+      
+      image = Docker::Image.build("FROM alpine\nRUN echo 'Hello, World!' > /test.txt ")
+      container = Docker::Container.create("Image": image.id)
+      container.stop
+
+      delete "/v1/containers/#{container.id}"
+      expect(response.status).to eq(200)
+      image.remove(:force => true)
+
+    end
+
+    it "returns success and delete the container if the container exists and is running" do 
+      
+      image = Docker::Image.build("FROM alpine\nRUN echo 'Hello, World!' > /test.txt ")
+      container = Docker::Container.create("Image": image.id)
+
+      delete "/v1/containers/#{container.id}"
+      expect(response.status).to eq(200)
+      image.remove(:force => true)
+
+    end
+    
   end
 end
