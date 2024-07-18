6ed67910b8e997341af41822b8cbf1902e57b053
pablo <pablobuntu@gmail.com>
2024-07-18 13:58:09 +0200

    Handle images errors

--- app/controllers/v1/images_controller.rb
+++ app/controllers/v1/images_controller.rb
@@ -1,18 +1,21 @@
 class V1::ImagesController < ApplicationController
 
   def index
-    images = Docker::Image.all
-    images.map! { |image| {id: image.id, tag: image.tag} }
-    render json: { status: { code: 200 }, data: images }
+    begin
+      images = Docker::Image.all
+      images.map! { |image| {id: image.id, tag: image.tag} }
+      render json: { status: { code: 200 }, data: images }
+    rescue => error
+      render json: { status: { code: 503 }, message: error.message  }, status: 503
+    end
   end
 
 
   def create
     begin
-    image = Docker::Image.build(request.raw_post)
-    render json: { status: { code: 200 }, data: {image: image.id} }
+      image = Docker::Image.build(request.raw_post)
+      render json: { status: { code: 200 }, data: {image: image.id} }
     rescue => error
-      puts error.message
       render json: { status: { code: 503 }, message: error.message  }, status: 503
     end
   end
